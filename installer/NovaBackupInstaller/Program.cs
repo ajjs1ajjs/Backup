6aed1b2c9cb96e12e0030fa4c2c1b47aa3289c31
Yaroslav Andreichuk <yaroslav.andreichuk@gmail.com>
2026-03-10 16:16:39 +0200

    "Added-GUI-Installer-Inno-Setup-WinForms

---
+++ installer/NovaBackupInstaller/Program.cs
@@ -0,0 +1,1 @@
+using System; using System.Diagnostics; using System.IO; using System.Windows.Forms; namespace NovaBackupInstaller { static class Program { [STAThread] static void Main() { Application.EnableVisualStyles(); Application.SetCompatibleTextRenderingDefault(false); Application.Run(new InstallerForm()); } } } 
