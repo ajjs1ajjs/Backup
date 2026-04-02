a981416ba44a49b41286570f2f7369a89d40dd85
Yaroslav Andreichuk <andreichuk.y@it-enterprise.com>
2026-04-02 15:43:36 +0300

    Disable rate limiting for development

--- src/server/Backup.Server/Program.cs
+++ src/server/Backup.Server/Program.cs
@@ -102,9 +102,10 @@ try
         options.GeneralRules = new List<RateLimitRule>
         {
             new() { Endpoint = "*", Period = "1m", Limit = 100 },
-            new() { Endpoint = "*/api/auth/*", Period = "1m", Limit = 20 },
-            new() { Endpoint = "*/api/*", Period = "1m", Limit = 100 }
+            new() { Endpoint = "*/api/auth/*", Period = "1m", Limit = 100 },
+            new() { Endpoint = "*/api/*", Period = "1m", Limit = 200 }
         };
+        options.IpWhitelist = new List<string> { "127.0.0.1", "::1" };
     });
     builder.Services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
     builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
@@ -141,7 +142,7 @@ try
     app.UseStaticFiles();
     app.UseAuthentication();
     app.UseAuthorization();
-    app.UseIpRateLimiting();
+    // app.UseIpRateLimiting();
 
     app.MapGrpcService<AgentServiceImpl>();
     app.MapGrpcService<JobServiceImpl>();
